5291ad499f7e4ecc18a5cc0b51ff57ce665bee70
davincidreams <lisamegawatts@gmail.com>
2023-04-27 22:31:11 -0400

    hero

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -14,19 +14,18 @@ export default function Hero() {
   return (
     <HeroWrapper>
       <Contents>
-        <CustomOverTitle>the coolest, saas product you have ever seen</CustomOverTitle>
-        <Heading>Make your life easier with our SaaS</Heading>
+        <CustomOverTitle>the coolest AI applications you have ever seen</CustomOverTitle>
+        <Heading>Make your job easier with AI</Heading>
         <Description>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, tempora qui. Explicabo voluptate iure ipsum molestias
-          repudiandae perspiciatis nostrum praesentium, unde pariatur tempora magni rem. Necessitatibus facilis obcaecati ratione.
+          Atlas Foundation is proud to offer a variety of AI services designed to make your job easier, from large language models, computer vision, image generation, digital twins, and Atlas Avatars AI guides.  
         </Description>
         <CustomButtonGroup>
           <Button onClick={() => setIsModalOpened(true)}>
-            Subscribe to the newsletter <span>&rarr;</span>
+            Sign Up Now  <span>&rarr;</span>
           </Button>
           <NextLink href="#whitepaper" passHref>
             <Button transparent>
-              Features <span>&rarr;</span>
+              AI/ML Services <span>&rarr;</span>
             </Button>
           </NextLink>
         </CustomButtonGroup>
